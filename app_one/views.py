f0c6f691b54383f8bfde1ec79a02761dbb610bbe
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2020-12-21 15:19:28 -0600

    add random_word functionality

--- app_one/views.py
+++ app_one/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import random
+import string
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def random_word(length):
+    letters = string.ascii_uppercase
+    word = ''.join(random.choice(letters) for i in range(length))
+    return word
